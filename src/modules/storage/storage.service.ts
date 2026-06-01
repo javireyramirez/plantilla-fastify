e05c374686ef80327473d1927efcac0ca6c9aba9
javireyramirez <73899426+javireyramirez@users.noreply.github.com>
2026-06-02 00:36:48 +0200

    storage rbac

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -200,7 +200,7 @@ export class StorageService extends BaseRbacService<any> {
         entityType,
         entityId,
         isPublic: fileData.isPublic,
-        userId: userId,
+        createdBy: userId,
       },
     });
 
